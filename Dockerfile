7114f481a9d5538bddd8108487a63d7bc9834a65
Pablo Miralles <pablomiralles99@gmail.com>
2024-11-06 14:36:36 +0100

    Minor mods

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,41 @@
 FROM ruby:latest
 ENV DEBIAN_FRONTEND noninteractive
 
-LABEL MAINTAINER Amir Pourmand
+LABEL authors="Amir Pourmand,George Araújo" \
+      description="Docker image for al-folio academic template" \
+      maintainer="Amir Pourmand"
 
-RUN apt-get update -y && apt-get install -y --no-install-recommends \
-    locales \
-    imagemagick \
-    build-essential \
-    zlib1g-dev \
-    jupyter-nbconvert \
-    inotify-tools procps && \
-    apt-get clean && rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*
+RUN apt-get update -y && \
+    apt-get install -y --no-install-recommends \
+        build-essential \
+        curl \
+        imagemagick \
+        inotify-tools \
+        locales \
+        nodejs \
+        procps \
+        python3-pip \
+        zlib1g-dev && \
+    pip --no-cache-dir install --upgrade --break-system-packages nbconvert
 
+# clean up
+RUN apt-get clean && \
+apt-get autoremove && \
+rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*  /tmp/*
 
+# set the locale
 RUN sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen && \
     locale-gen
 
+# Fix imagemagick policy
+RUN sed -i.bak '/<policy domain="coder" rights="none" pattern="PDF" \/>/s|^|<!-- |; s|$| -->|' /etc/ImageMagick-6/policy.xml
 
-ENV LANG=en_US.UTF-8 \
+
+ENV EXECJS_RUNTIME=Node \
+    JEKYLL_ENV=production \
+    LANG=en_US.UTF-8 \
     LANGUAGE=en_US:en \
-    LC_ALL=en_US.UTF-8 \
-    JEKYLL_ENV=production
+    LC_ALL=en_US.UTF-8
 
 RUN mkdir /srv/jekyll
 
@@ -30,9 +45,9 @@ ADD Gemfile /srv/jekyll
 WORKDIR /srv/jekyll
 
 # install jekyll and dependencies
-RUN gem install jekyll bundler
-
+RUN gem install --no-document jekyll bundler
 RUN bundle install --no-cache
+
 # && rm -rf /var/lib/gems/3.1.0/cache
 EXPOSE 8080
 
--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -1,28 +1,69 @@
 #!/bin/bash
 
-CONFIG_FILE=_config.yml 
+# CONFIG_FILE=_config.yml 
+
+# # OPTIONS=""
+# OPTIONS="--draft"
+# # OPTIONS="--future --unpublished --draft"
+
+# COMMAND="jekyll serve $OPTIONS --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"
+
+# /bin/bash -c "rm -f Gemfile.lock && exec $COMMAND"&
+
+# while true; do
+
+#   inotifywait -q -e modify,move,create,delete $CONFIG_FILE
+
+#   if [ $? -eq 0 ]; then
+ 
+#     echo "Change detected to $CONFIG_FILE, restarting Jekyll"
+
+#     jekyll_pid=$(pgrep -f jekyll)
+#     kill -KILL $jekyll_pid
+
+#     /bin/bash -c "rm -f Gemfile.lock && exec $COMMAND"&
+
+#   fi
+
+# done
+
+# ----------------------------------------------------------------------------------------------  
+set -euo pipefail
+
+echo "Entry point script running"
+
+CONFIG_FILE=_config.yml
+
+# Function to manage Gemfile.lock
+manage_gemfile_lock() {
+    git config --global --add safe.directory '*'
+    if command -v git &> /dev/null && [ -f Gemfile.lock ]; then
+        if git ls-files --error-unmatch Gemfile.lock &> /dev/null; then
+            echo "Gemfile.lock is tracked by git, keeping it intact"
+            git restore Gemfile.lock 2>/dev/null || true
+        else
+            echo "Gemfile.lock is not tracked by git, removing it"
+            rm Gemfile.lock
+        fi
+    fi
+}
 
 # OPTIONS=""
 OPTIONS="--draft"
 # OPTIONS="--future --unpublished --draft"
+start_jekyll() {
+    manage_gemfile_lock
+    bundle exec jekyll serve $OPTIONS --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling &
+}
 
-COMMAND="jekyll serve $OPTIONS --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"
-
-/bin/bash -c "rm -f Gemfile.lock && exec $COMMAND"&
+start_jekyll
 
 while true; do
-
-  inotifywait -q -e modify,move,create,delete $CONFIG_FILE
-
-  if [ $? -eq 0 ]; then
- 
-    echo "Change detected to $CONFIG_FILE, restarting Jekyll"
-
-    jekyll_pid=$(pgrep -f jekyll)
-    kill -KILL $jekyll_pid
-
-    /bin/bash -c "rm -f Gemfile.lock && exec $COMMAND"&
-
-  fi
-
+    inotifywait -q -e modify,move,create,delete $CONFIG_FILE
+    if [ $? -eq 0 ]; then
+        echo "Change detected to $CONFIG_FILE, restarting Jekyll"
+        jekyll_pid=$(pgrep -f jekyll)
+        kill -KILL $jekyll_pid
+        start_jekyll
+    fi
 done
